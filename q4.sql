74ddaf1df3d6a8a54121f0449f4e12f37b64573b
anudeepbutha <111654636+anudeepbutha@users.noreply.github.com>
2025-08-16 22:03:22 +0530

    Update q4.sql

--- q4.sql
+++ q4.sql
@@ -6,11 +6,22 @@ DROP PROCEDURE IF EXISTS SendWatchTimeReport;
 DELIMITER //
 CREATE PROCEDURE GetWatchHistoryBySubscriber()
 BEGIN
-    SELECT s.subscribername , SUM(wh.watchtime)
+    DECLARE subid INT;
+    DECLARE finish INT DEFAULT 0;
+    DECLARE subscriberlist CURSOR FOR SELECT DISTINCT subscriberid FROM watchhistory;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET finish = 1;
+    OPEN subscriberlist;
+    read_loop: LOOP
+        FETCH subscriberlist INTO subid;
+        IF finish = 1 THEN
+            LEAVE read_loop;
+		END IF;
+    SELECT s.subscriberid, s.subscribername , SUM(wh.watchtime)
     FROM subscribers s, watchhistory wh
     WHERE s.subscriberid = wh.subscriberID 
-    GROUP BY wh.subscriberID 
-    HAVING SUM(wh.watchtime);
+    GROUP BY wh.subscriberID;
+    END LOOP read_loop;
+    CLOSE subscriberlist;
 END //
 
 CREATE PROCEDURE SendWatchTimeReport()
